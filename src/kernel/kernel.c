8f47ce9568492829824802cc68b7b399b6c02757
wxwsk8er <officialyoutextcontrol@gmail.com>
2015-02-13 18:40:04 -0500

    Updating Sources

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -43,7 +43,7 @@ void kernel(struct multiboot *mboot_ptr)
     
     
     DEBUG("Allocating memory for Initial Ramdisk location object...");
-    u32int initrd_location = malloc(sizeof(mboot_ptr->mods_addr));   //mboot_ptr->mods_addr;//malloc(sizeof(mboot_ptr->mods_addr));
+    u32int initrd_location = mboot_ptr->mods_addr;   //mboot_ptr->mods_addr;//malloc(sizeof(mboot_ptr->mods_addr));
                                                                      //PRINT_HEX("Memory allocation successful. Location = ", initrd_location);
     
     PRINT("Allocating memory for Initial Ramdisk end location object...");
